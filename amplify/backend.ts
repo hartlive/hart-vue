20404c8b85d58ce35fe1371e60e2cb5d4a73cd39
jef <cedar0822@163.com>
2025-08-10 14:29:24 +0800

    commit

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,34 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+backend.addOutput({
+  storage: {
+    aws_region: 'eu-central-1',
+    bucket_name: 'hart-eu',
+    
+    buckets: [
+      {
+        aws_region: 'eu-central-1',
+        bucket_name: 'hart-eu',
+        name: 'hart-eu',
+        policy: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Effect: 'Allow',
+              Principal: '*',
+              Action: 's3:GetObject',
+              Resource: 'arn:aws:s3:::hart-eu/*'
+            }
+          ]
+        }
+      }
+    ]
+  }
+})
+
