a10c3a58789b0a2dc06abeda7b1ac4ab30408bde
jef <cedar0822@163.com>
2025-08-10 15:58:38 +0800

    commit

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,13 +1,18 @@
 import { defineStorage } from '@aws-amplify/backend';
-import { Bucket } from '@aws-cdk/aws-s3';
-import { Construct } from '@aws-cdk/core';
 
 export const storage = defineStorage({
   name: 'hart-eu',
-  cdk: {
-    bucket: (scope: Construct) => {
-      // 引用已存在的桶（需确保桶在当前AWS账号和区域中存在）
-      return Bucket.fromBucketName(scope, 'ExistingHartBucket', 'hart-eu');
-    }
-  }
+  access: (allow) => ({
+    'public/*': [
+      allow.guest.to(['read', 'write']),
+      allow.authenticated.to(['read', 'write']),
+    ],
+    'protected/{entity_id}/*': [
+      allow.authenticated.to(['read', 'write']),
+      allow.entity('identity').to(['read', 'write'])
+    ],
+    'private/{entity_id}/*': [
+      allow.entity('identity').to(['read', 'write'])
+    ]
+  })
 });
