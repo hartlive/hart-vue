fda35c5e7eda1a0d531dee970074c9803f150e1a
jef <cedar0822@163.com>
2025-08-10 15:54:52 +0800

    commit

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -9,18 +9,5 @@ export const storage = defineStorage({
       // 引用已存在的桶（需确保桶在当前AWS账号和区域中存在）
       return Bucket.fromBucketName(scope, 'ExistingHartBucket', 'hart-eu');
     }
-  },
-  access: (allow) => ({
-    'public/*': [
-      allow.guest.to(['read', 'write']),
-      allow.authenticated.to(['read', 'write']),
-    ],
-    'protected/{entity_id}/*': [
-      allow.authenticated.to(['read', 'write']),
-      allow.entity('identity').to(['read', 'write'])
-    ],
-    'private/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write'])
-    ]
-  })
+  }
 });
