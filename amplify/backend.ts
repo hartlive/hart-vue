fe479e52fdbc24df6659fb20d1de3fc0e8dca2a6
jef <cedar0822@163.com>
2025-08-10 15:36:50 +0800

    commit

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,8 +3,7 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 const storage = defineStorage({
-  name: 'hart-eu',
-  access: 'private' // 先以私有方式创建
+  name: 'hart-eu'
 });
 
 const backend = defineBackend({
